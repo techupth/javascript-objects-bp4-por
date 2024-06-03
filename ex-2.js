e4f8d0cce9439d10f60f0bc7aeb4febccec37788
Por <Poramaporn2540@gmail.com>
2024-06-03 11:22:42 +0700

    feat(exercise): Add complete exercises

--- ex-2.js
+++ ex-2.js
@@ -8,3 +8,14 @@ const product = {
 };
 let socialMediaShare = "facebookShare";
 // Start code here
+
+//Reassign_edit
+product.ratings = 4.5;
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+product[code] = "IT0101010";
+product.price = 6000;
+//Delete
+delete product.ratings;
+
+//create new key-value
+product[socialMediaShare] = 45.5;
